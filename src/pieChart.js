aff6345d5dcf3b6048faf44b3b2ef75b37f8b82d
MUHAMMAD WASEEM ARIF <wasimarif602@gmail.com>
2021-05-25 10:08:04 +0500

    edited piechart

--- src/pieChart.js
+++ src/pieChart.js
@@ -2,47 +2,22 @@ import React from "react";
 import { Pie } from "react-chartjs-2";
 
 const data = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  labels: ["Red", "Blue", "Yellow"],
   datasets: [
     {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 1,
+      data: [300, 50, 100],
+      backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+      hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
     },
   ],
 };
 
-const PieChart = () => (
-  <>
-    <div className="header">
-      <h1 className="title">Pie Chart</h1>
-      <div className="links">
-        <a
-          className="btn btn-gh"
-          href="https://github.com/reactchartjs/react-chartjs-2/blob/master/example/src/charts/Pie.js"
-        >
-          Github Source
-        </a>
-      </div>
+function PieChart() {
+  return (
+    <div>
+      <Pie data={data} height={60} />
     </div>
-    <Pie data={data} />
-  </>
-);
+  );
+}
 
 export default PieChart;
